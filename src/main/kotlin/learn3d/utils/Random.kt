3fb1c15cd7a0198d6f6b374456c051073ab79ebb
utikeev <utikeev@gmail.com>
2018-10-12 13:12:27 +0300

    Fix random functions

--- src/main/kotlin/learn3d/utils/Random.kt
+++ src/main/kotlin/learn3d/utils/Random.kt
@@ -2,6 +2,6 @@ package learn3d.utils
 
 import java.util.concurrent.ThreadLocalRandom
 
-fun drand(from: Double = .0, to: Double = .1): Double = ThreadLocalRandom.current().nextDouble(from, to)
+fun drand(from: Double = .0, to: Double = 1.0): Double = ThreadLocalRandom.current().nextDouble(from, to)
 
-fun intrand(to: Int, from: Int = 0): Int = ThreadLocalRandom.current().nextInt(to)
+fun intrand(to: Int, from: Int = 0): Int = ThreadLocalRandom.current().nextInt(from, to)
